cb1a2d52f4686517c09c720b0bf7fd001c92cff9
Franz Greiling <dev.git@lc3dyr.de>
2014-10-28 10:50:06 +0100

    Initial Commit

---
+++ Barista/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Barista
+//
+//  Created by Franz Greiling on 28/10/14.
+//  Copyright (c) 2014 Franz Greiling. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
