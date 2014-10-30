94e44dae6c9c85de74cc8a4f1327a45fff2e37c3
Franz Greiling <dev.git@lc3dyr.de>
2014-10-30 12:06:58 +0100

    Code Cleanup and Basic Comments

--- PowerAssertion.swift
+++ PowerAssertion.swift
@@ -10,141 +10,141 @@ import Foundation
 
 import IOKit.pwr_mgt
 
+
 enum PowerAssertionType: String {
     case PreventUserIdleSystemSleep = "PreventUserIdleSystemSleep",
-     PreventUserIdleDisplaySleep = "PreventUserIdleDisplaySleep",
-     PreventSystemSleep = "PrevenSystemSleep",
-     NoIdleSleep = "NoIdleSleepAssertion",
-     NoDisplaySleep = "NoDisplaySleepAssertion"
+    PreventUserIdleDisplaySleep = "PreventUserIdleDisplaySleep",
+    PreventSystemSleep = "PrevenSystemSleep",
+    NoIdleSleep = "NoIdleSleepAssertion",
+    NoDisplaySleep = "NoDisplaySleepAssertion"
 }
 
 enum PowerAssertionDictionaryKey: String {
     case AssertionTimeoutKey = "TimeoutSeconds",
-        AssertionTimeoutActionKey = "TimeoutAction",
-        AssertionTimeoutActionLog = "TimeoutActionLog",
-        AssertionTimeoutActionTurnOff = "TimeoutActionTurnOff",
-        AssertionTimeoutActionRelease = "TimeoutActionRelease",
-        AssertionRetainCountKey = "RetainCount",
-        AssertionNameKey = "AssertName",
-        AssertionDetailsKey = "Details",
-        AssertionHumanReadableReasonKey = "HumanReadableReason",
-        AssertionLocalizationBundlePathKey = "BundlePath",
-        AssertionFrameworkIDKey = "FrameworkBundleID",
-        AssertionPlugInIDKey = "PlugInBundleID",
-        AssertionTypeKey = "AssertType",
-        AssertionLevelKey = "AssertLevel"
+    AssertionTimeoutActionKey = "TimeoutAction",
+    AssertionTimeoutActionLog = "TimeoutActionLog",
+    AssertionTimeoutActionTurnOff = "TimeoutActionTurnOff",
+    AssertionTimeoutActionRelease = "TimeoutActionRelease",
+    AssertionRetainCountKey = "RetainCount",
+    AssertionNameKey = "AssertName",
+    AssertionDetailsKey = "Details",
+    AssertionHumanReadableReasonKey = "HumanReadableReason",
+    AssertionLocalizationBundlePathKey = "BundlePath",
+    AssertionFrameworkIDKey = "FrameworkBundleID",
+    AssertionPlugInIDKey = "PlugInBundleID",
+    AssertionTypeKey = "AssertType",
+    AssertionLevelKey = "AssertLevel"
 }
 
 enum PowerAssertionLevel: UInt32 {
-    case On = 255
-    case Off =  0
+    case On = 255,
+    Off =   0
 }
 
+
+/// Wrapper around the IOPMlib C-API
 class PowerAssertion {
     
     private var assertionID: IOPMAssertionID = UInt32(kIOPMNullAssertionID)
     
+    /*
+    *  Wrapper around the AssertionDictionary Values
+    */
     var name: String {
-        set(name) {
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionNameKey,
-                theValue: name
-            )
-        }
         get {
-            return getAssertionProperty(PowerAssertionDictionaryKey.AssertionNameKey)! as String
+            return getAssertionProperty(.AssertionNameKey)! as String
+        }
+        set(name) {
+            setAssertionProperty(.AssertionNameKey, theValue: name)
         }
     }
+    
     var type: PowerAssertionType {
         get {
-            return PowerAssertionType(rawValue:
-                getAssertionProperty(PowerAssertionDictionaryKey.AssertionTypeKey)! as String
-            )!
+            return PowerAssertionType(rawValue: getAssertionProperty(.AssertionTypeKey)! as String)!
         }
     }
+    
     var level: PowerAssertionLevel {
+        get {
+            return PowerAssertionLevel(rawValue: UInt32(getAssertionProperty(.AssertionLevelKey)! as Int))!
+        }
         set(level) {
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionLevelKey,
-                theValue: NSNumber(unsignedInt: level.rawValue)
-            )
-        }
-        get {
-            return PowerAssertionLevel(rawValue:
-                UInt32(getAssertionProperty(PowerAssertionDictionaryKey.AssertionLevelKey)! as Int)
-            )!
+            setAssertionProperty(.AssertionLevelKey, theValue: NSNumber(unsignedInt: level.rawValue))
         }
     }
+    
     var details: String? {
+        get {
+            return getAssertionProperty(.AssertionDetailsKey) as String?
+        }
         set(details) {
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionDetailsKey,
-                theValue: details
-            )
-        }
-        get {
-            return getAssertionProperty(PowerAssertionDictionaryKey.AssertionDetailsKey) as String?
+            setAssertionProperty(.AssertionDetailsKey, theValue: details)
         }
     }
+    
     var humanReadableReason: String? {
+        get {
+            return getAssertionProperty(.AssertionHumanReadableReasonKey) as String?
+        }
         set(hRString) {
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionHumanReadableReasonKey,
-                theValue: hRString
-            )
-        }
-        get {
-            return getAssertionProperty(PowerAssertionDictionaryKey.AssertionHumanReadableReasonKey) as String?
+            setAssertionProperty(.AssertionHumanReadableReasonKey, theValue: hRString)
         }
     }
+    
     var localizationBundlePath: String? {
+        get {
+            return getAssertionProperty(.AssertionLocalizationBundlePathKey) as String?
+        }
         set(lBPath) {
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionLocalizationBundlePathKey,
-                theValue: lBPath
-            )
-        }
-        get {
-            return getAssertionProperty(PowerAssertionDictionaryKey.AssertionLocalizationBundlePathKey) as String?
+            setAssertionProperty(.AssertionLocalizationBundlePathKey, theValue: lBPath)
         }
     }
+    
     var timeoutInSeconds: Double? {
-        set(timeout) {
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionTimeoutKey,
-                theValue: timeout
-            )
-            setAssertionProperty(
-                PowerAssertionDictionaryKey.AssertionTimeoutActionKey,
-                theValue: PowerAssertionDictionaryKey.AssertionTimeoutActionTurnOff.rawValue
-            )
-        }
         get {
-            return getAssertionProperty(PowerAssertionDictionaryKey.AssertionTimeoutKey) as Double?
+            return getAssertionProperty(.AssertionTimeoutKey) as Double?
+        }
+        set(timeout) {
+            // TODO: Setting the Timeout AND a default value for TimeoutAction
+            //   This should probably not be done
+            setAssertionProperty(.AssertionTimeoutKey, theValue: timeout)
+            setAssertionProperty(.AssertionTimeoutActionKey, theValue: PowerAssertionDictionaryKey.AssertionTimeoutActionTurnOff.rawValue)
         }
     }
     
-    
+    /*
+    *  Init
+    */
     init?(name: String, type: PowerAssertionType, level: PowerAssertionLevel) {
-        
         let ret: IOReturn = IOPMAssertionCreateWithName(
             type.rawValue,
             level.rawValue,
             name,
             &assertionID)
         
-        // Because for some reason, 
+        // Because for some reason,
         // AssertionCreateWithName does not honor IOPMAssertionLevel
         if level == .Off {
             self.level = .Off
         }
-                
+        
         if ret != kIOReturnSuccess {
             return nil
         }
     }
     
+    deinit {
+        let ret: IOReturn = IOPMAssertionRelease(assertionID)
+        
+        if ret != kIOReturnSuccess {
+            fatalError("Unable to release Assertion!")
+        }
+    }
     
+    /*
+    *  Private Helpers
+    */
     private func setAssertionProperty(theProperty: PowerAssertionDictionaryKey, theValue: AnyObject?) -> IOReturn {
         return IOPMAssertionSetProperty(assertionID, theProperty.rawValue, theValue)
     }
@@ -154,17 +154,10 @@ class PowerAssertion {
         return dict.valueForKey(theProperty.rawValue)
     }
     
-    deinit {
-        let ret: IOReturn = IOPMAssertionRelease(assertionID)
-
-        if ret != kIOReturnSuccess {
-            fatalError("Unable to release Assertion!")
-        }
-    }
     
-    /***
-     * Class Functions
-     ***/
+    /*
+    *  Class Functions
+    */
     class func getAssertionStatus() -> Dictionary<String,Int>? {
         var _assertionsStatus: Unmanaged<CFDictionaryRef>?
         
