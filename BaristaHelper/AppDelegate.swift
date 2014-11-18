76871ca9c6e7b9918801668b0c5bd3bd4ee88036
Franz Greiling <dev.git@lc3dyr.de>
2014-11-18 12:13:58 +0100

    Add new target BaristaHelper to project

---
+++ BaristaHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BaristaHelper
+//
+//  Created by Franz Greiling on 18.11.2014.
+//  Copyright (c) 2014 Franz Greiling. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BaristaHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BaristaHelper
+//
+//  Created by Franz Greiling on 18.11.2014.
+//  Copyright (c) 2014 Franz Greiling. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
