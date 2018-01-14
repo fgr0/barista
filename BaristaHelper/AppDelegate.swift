6838a60365bcded3c544e91a3787d8e9bec3e028
Franz Greiling <fgr@sadmac.de>
2018-01-14 04:39:54 +0100

    Refactor Helper App

--- BaristaHelper/AppDelegate.swift
+++ BaristaHelper/AppDelegate.swift
@@ -13,23 +13,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     // MARK: - NSApplicationDelegate Protocol
     func applicationWillFinishLaunching(_ aNotification: Notification) {
-        // Since Helper must reside in Bundle/Contents/Library/LoginItems
-        // remove unnessesary path elements
-        let appPath = ((((NSURL(fileURLWithPath: Bundle.main.bundlePath) as NSURL)
-            .deletingLastPathComponent as NSURL?)?.deletingLastPathComponent as NSURL?)?
-            .deletingLastPathComponent?.deletingLastPathComponent().path)!
-        let appBundle = Bundle(path: appPath)
-        var isRunning = false
-        
-        for app in NSWorkspace.shared.runningApplications {
-            if app.bundleIdentifier == appBundle?.bundleIdentifier {
-                isRunning = true
-                break
-            }
+        // Since Helper must reside in Bundle/Contents/Library/LoginItems,
+        // so remove unnessesary path elements
+        var path = Bundle.main.bundlePath as NSString
+        for _ in 1...4 {
+            path = path.deletingLastPathComponent as NSString
         }
+        
+        let mainBundle = Bundle(path: path as String)
 
-        if !isRunning {
-            NSWorkspace.shared.launchApplication(appPath)
+        if !NSWorkspace.shared.runningApplications.contains { $0.bundleIdentifier == mainBundle?.bundleIdentifier } {
+            NSWorkspace.shared.launchApplication(path as String)
         }
         
         NSApplication.shared.terminate(nil)
